722af6de95a570951dd203ccd63c5b8e93513628
Alex <tictak211@gmail.com>
2020-11-27 21:01:31 +0200

    movies service

--- movies-frontend/src/app/app.component.ts
+++ movies-frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   ThemeVariables,
   ThemeRef,
@@ -7,6 +7,8 @@ import {
   LyTheme2,
 } from '@alyle/ui';
 import { ThemeMinimaDark } from '@alyle/ui/themes/minima';
+import { MoviesService } from './services/movies/movies.service';
+import { Observable } from 'rxjs';
 
 const STYLES = (theme: ThemeVariables, ref: ThemeRef) => {
   // eslint-disable-next-line no-underscore-dangle
@@ -33,10 +35,15 @@ const STYLES = (theme: ThemeVariables, ref: ThemeRef) => {
   styleUrls: ['./app.component.scss'],
   providers: [StyleRenderer],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   readonly classes = this.sRenderer.renderSheet(STYLES, true);
+  movies$: Observable<any> | undefined;
 
-  constructor(readonly sRenderer: StyleRenderer, private theme: LyTheme2) {}
+  constructor(
+    readonly sRenderer: StyleRenderer,
+    private theme: LyTheme2,
+    private readonly movieService: MoviesService,
+  ) {}
 
   setTheme() {
     if (this.theme.variables.name === 'minima-light') {
@@ -45,5 +52,9 @@ export class AppComponent {
     }
 
     this.theme.setTheme('minima-light');
+  }
+
+  ngOnInit(): void {
+    this.movies$ = this.movieService.getMovies();
   }
 }
