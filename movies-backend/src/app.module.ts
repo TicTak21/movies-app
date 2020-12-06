316c29ba180d66db8f44b866b957033941c6582f
Alex <tictak211@gmail.com>
2020-12-06 14:49:55 +0200

    backend: env fix

--- movies-backend/src/app.module.ts
+++ movies-backend/src/app.module.ts
@@ -11,9 +11,6 @@ import { MovieModule } from './movie/movie.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
-// === configs ===
-import config from './mikro-orm.config';
-
 // === shared ===
 import { HttpErrorFilter } from './shared/filters/http-error.filter';
 import { LoggingInterceptor } from './shared/interceptors/logging.interceptor';
