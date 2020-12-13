ae01da73f6da95ddf2c4ca7ef9d49eacbb6c6436
Alex <tictak211@gmail.com>
2020-12-13 18:44:43 +0200

    backend: hide secret

--- movies-backend/src/auth/auth.module.ts
+++ movies-backend/src/auth/auth.module.ts
@@ -6,14 +6,18 @@ import { LocalStrategy } from '../shared/strategies/local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from 'src/shared/strategies/jwt.stategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'asdf',
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
