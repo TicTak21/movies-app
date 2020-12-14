60211ec3c3892ff2d8d2aa93618d2677d189e089
Alex <tictak211@gmail.com>
2020-12-14 12:29:23 +0200

    backend: redirect from `/` to `/api`

--- movies-backend/src/app.controller.ts
+++ movies-backend/src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Index')
 @Controller()
-export class AppController {}
+export class AppController {
+  @Get()
+  @Redirect('/api', 301)
+  index() {
+    return '';
+  }
+}
